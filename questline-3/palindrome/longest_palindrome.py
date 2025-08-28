c0b3752d2801907a0a24d44cabe4da83d54646f1
Dharun18-07 <a.dharun2007@gmail.com>
2025-08-28 16:04:16 +0530

    Create longest_palindrome.py

---
+++ questline-3/palindrome/longest_palindrome.py
@@ -0,0 +1,10 @@
+#Longest Palindrome
+s='ababdududbaab'
+p = ""
+for i in range(len(s)):
+    for j in range(i, len(s)):
+        sub = s[i:j + 1]
+        if sub == sub[::-1] and len(sub) > len(p):
+            p = sub
+print(p)
+#output- abdududba
