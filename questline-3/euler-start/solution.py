542cf0c9e69d4b0ff2f440c92678adcd2c65a7bc
Dharun18-07 <a.dharun2007@gmail.com>
2025-08-28 15:15:23 +0530

    Update solution.py

--- questline-3/euler-start/solution.py
+++ questline-3/euler-start/solution.py
@@ -1,10 +1,18 @@
-#q1
+#Problem 1 - Multiples of 3 or 5
 n=1000
 sum=0
 for i in range(1,n):
     if i%3==0 or i%5==0:
         sum=sum+i
-print(sum)
+print('Sum of all multiple of 3 or 5 below 1000 is',sum)
 
-
+#Problem 6 -Sum Square Difference
+sum1=0
+sum2=0
+for i in range(1,101):
+    sum1=sum1+i
+sum1=sum1**2
+for i in range(1,101):
+    sum2=sum2+i*i
+print('The difference between the sum of the squares of the first one hundred natural numbers and the square of the sum is',sum1-sum2)
 
